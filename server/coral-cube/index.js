5673f075f120d5fd4cefa8f9feec563253a997ce
spiritbroo <rinoakbr@gmail.com>
2022-12-05 10:22:37 +0700

    feat(server/coral-cube): update program info and state files with new data 🌀

--- server/coral-cube/index.js
+++ server/coral-cube/index.js
@@ -1,6 +1,5 @@
 const {App, Deta} = require('deta')
 const express = require('express')
-const axios = require("axios")
 const app = App(express())
 const deta = Deta('c08piu78_Ho49cFbttrmc1vHW59rM89F6duMjCjh2')
 const cors = require("cors")
@@ -29,85 +28,7 @@ app.post('/coral', async (req, res) => {
 
 })
 
-function timeout(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms))
-}
-
-function setTimeToZero(timestamp) {
-  // Parse the timestamp string using the Date object
-  var date = new Date(timestamp)
-
-  // Set the hours, minutes, and seconds to 0 using the setUTCHours, setUTCMinutes, and setUTCSeconds methods
-  date.setUTCHours(0)
-  date.setUTCMinutes(0)
-  date.setUTCSeconds(0)
-
-  // Convert the date back to a string in ISO format using the toISOString method
-  return encodeURIComponent(date.toISOString()
-    .replace(".000Z", ""))
-}
-
-async function getData(updateAuthority, symbol, before) {
-  try {
-    const {data} = await axios.get(`https://api.coralcube.cc/0dec5037-f67d-4da8-9eb6-97e2a09ffe9a/inspector/getMintActivities?update_authority=${updateAuthority}&collection_symbol=${symbol}${before ? "&before=" + setTimeToZero(before) : ""}`)
-    //    console.log(data)
-    return data
-  } catch (e) {
-    console.log(e.message)
-    return []
-  }
-  //    console.log(`https://api.coralcube.cc/0dec5037-f67d-4da8-9eb6-97e2a09ffe9a/inspector/getMintActivities?limit=10&update_authority=${updateAuthority}&collection_symbol=${symbol}${before ? "&before=" + before : ""}`)
-
-}
-
-async function cobaCron() {
-  let ress = await collectionList.fetch({})
-  let allItems = ress.items
 
 
-  // continue fetching until last is not seen
-  while (ress.last) {
-    ress = await collectionList.fetch({}, {last: ress.last})
-    allItems = allItems.concat(ress.items)
-
-  }
-  allItems = allItems.filter(({updateAuthority, symbol}) => updateAuthority && symbol)
-  if (allItems.length === 0) return
-  for (let i = 0; i < allItems.length; i++) {
-
-    let data = await getData(allItems[i].updateAuthority, allItems[i].symbol)
-    let data2 = data
-
-    data.forEach(async (item) => {
-      const {signature} = item
-      await royalty.put({...item, collectionName: allItems[i].symbol}, signature)
-    })
-
-
-    while (data2.length > 0) {
-      await timeout(1000)
-      const date = data2[data2.length - 1].time
-      console.log(setTimeToZero(date))
-
-      data2 = await getData(allItems[i].updateAuthority, allItems[i].symbol, date)
-      data2.forEach(async (item) => {
-        const {signature} = item
-        await royalty.put({...item, collectionName: allItems[i].symbol}, signature)
-      })
-      if (data2.length === 0) {
-        break
-
-      }
-    }
-
-
-  }
-  console.log("sukses")
-  return ress.length
-}
-
-app.lib.cron(async () => {
-  await cobaCron()
-})
 
 module.exports = app
